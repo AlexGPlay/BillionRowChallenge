9789a1ffba7e04d47f86bfb4a4578c0fd5af80bc
alexgplay <alexgp1234@gmail.com>
2024-03-18 15:38:33 +0100

    Move buffering to utils

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ build-line-by-line: line_by_line.c
 	gcc -O3 line_by_line.c -o line_by_line.out
 time-line-by-line: build-line-by-line
 	time ./line_by_line.out
+build-buffering: buffering.c 
+	gcc -O3 buffering.c -o buffering.out
+time-buffering: build-line-by-line
+	time ./buffering.out
--- buffering.c
+++ buffering.c
@@ -2,143 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "./utils/data.c"
+
 #define BUFFER_SIZE 5*1024*1024
 #define INITIAL_MEASUREMENTS 1500
 
 #define DEBUG_LINES
-
-struct NamedNode{
-  char* name;
-  float min;
-  float max;
-  float sum;
-  int count;
-};
-
-struct NameLinkedList{
-  char* name;
-  struct NameLinkedList* next;
-};
-
-int hash(char* name, int size, int iterations){
-  int hash = 0;
-  for(int i = 0; i < strlen(name); i++){
-    hash += name[i];
-  }
-  return (hash + iterations) % size;
-}
-
-int find(char* name, struct NamedNode *measurements, int size){
-  for(int i = 0; i < size; i++){
-    int index = hash(name, size, i);
-    if(measurements[index].name == NULL){
-      return -1;
-    }
-
-    if(strcmp(measurements[index].name, name) == 0){
-      return index;
-    }
-  }
-
-  return -1;
-}
-
-int getAvailablePosition(char* name, struct NamedNode *measurements, int size){
-  for(int i = 0; i < size; i++){
-    int index = hash(name, size, i);
-    if(measurements[index].name == NULL){
-      return index;
-    }
-  }
-
-  return -1;
-}
-
-int add(char* name, int value, struct NamedNode* measurements, int size){
-  struct NamedNode newNode;
-  newNode.name = (char*)malloc(strlen(name) + 1 * sizeof(char));
-  strcpy(newNode.name, name);
-  newNode.min = value;
-  newNode.max = value;
-  newNode.sum = value;
-  newNode.count = 1;
-
-  int index = getAvailablePosition(name, measurements, size);
-  measurements[index] = newNode;
-
-  return index;  
-}
-
-int sortedAddToLinkedList(char* name, struct NameLinkedList** sortedMeasurementsNamesPtr){
-  struct NameLinkedList* sortedMeasurementsNames = *sortedMeasurementsNamesPtr;
-
-  if(sortedMeasurementsNames == NULL){
-    struct NameLinkedList* newNode = (struct NameLinkedList*)malloc(sizeof(struct NameLinkedList));
-    newNode->name = (char*)malloc(strlen(name) + 1 * sizeof(char));
-    strcpy(newNode->name, name);
-    newNode->next = NULL;
-
-    *sortedMeasurementsNamesPtr = newNode;
-
-    return 1;
-  }
-
-  struct NameLinkedList* newNode = (struct NameLinkedList*)malloc(sizeof(struct NameLinkedList));
-  newNode->name = (char*)malloc(strlen(name) + 1 * sizeof(char));
-  strcpy(newNode->name, name);
-  newNode->next = NULL;
-
-  if(strcmp(sortedMeasurementsNames->name, name) > 0){
-    newNode->next = sortedMeasurementsNames;
-    *sortedMeasurementsNamesPtr = newNode;
-
-    return 1;
-  }
-
-
-  struct NameLinkedList* current = sortedMeasurementsNames;
-  struct NameLinkedList* previous = NULL;
-  while(current != NULL){
-    if(strcmp(current->name, name) > 0){
-      newNode->next = current;
-      previous->next = newNode;
-
-      return 1;
-    }
-
-    previous = current;
-    current = current->next;
-  }
-
-  previous->next = newNode;
-
-  return -1;
-}
-
-void addMeasurement(char* name, float value, struct NamedNode **measurementsPtr, int* currentSize, int* insertedSize, struct NameLinkedList **sortedMeasurementsNamesPtr){
-  struct NamedNode *measurements = *measurementsPtr;
-
-  int index = find(name, measurements, *currentSize);
-  if(index == -1){
-    index = add(name, value, measurements, *currentSize);
-
-    sortedAddToLinkedList(name, sortedMeasurementsNamesPtr);
-
-    (*insertedSize)++;
-  } else {
-    if(value < measurements[index].min){
-      measurements[index].min = value;
-    }
-
-    if(value > measurements[index].max){
-      measurements[index].max = value;
-    }
-
-    measurements[index].sum += value;
-    measurements[index].count++;
-  }
-}
-
 
 int main(){
   char buffer[BUFFER_SIZE];
@@ -157,7 +26,7 @@ int main(){
 
   struct NameLinkedList *sortedMeasurementsNames = NULL;
 
-  FILE *file = fopen("measurements.txt", "r");
+  FILE *file = fopen("measurements_min.txt", "r");
 
   while (1) {
     size_t bytesRead = fread(buffer, 1, BUFFER_SIZE, file);
@@ -219,12 +88,7 @@ int main(){
 
   fclose(file);
 
-  struct NameLinkedList* current = sortedMeasurementsNames;
-  while(current != NULL){
-    int position = find(current->name, measurements, currentSize);
-    printf("%s: %f %f %f\n", current->name, measurements[position].min, measurements[position].max, measurements[position].sum / measurements[position].count);
-    current = current->next;
-  }
+  printMeasurements(sortedMeasurementsNames, measurements, insertedSize);
 
   return 0;
 }
